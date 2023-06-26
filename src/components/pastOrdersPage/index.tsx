11ef9b1ad0f3f0aa2d1c471d69c4586e86f0b4fa
Maxwell-Yang-2001 <migacanada@gmail.com>
2023-06-25 20:56:01 -0700

    Add past orders page structure

--- src/components/pastOrdersPage/index.tsx
+++ src/components/pastOrdersPage/index.tsx
@@ -3,7 +3,11 @@ import { PAGE } from "../../utils/constants";
 import TopNavigator from "../topNavigator";
 import { State, User } from "../../redux/state";
 import { connect } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "./past-orders-page.css";
+import mcdonalds from "../../assets/mcdonalds.jpeg";
+import { Button, Col, Row } from "react-bootstrap";
+import { QuickBgButton } from "../commons";
 
 const mapStateToProps = (state: State) => ({
   user: state.user,
@@ -24,5 +28,52 @@ export const PastOrdersPage = connect(
       navigate("/account");
     }
   }, []);
-  return <TopNavigator page={PAGE.Account} />;
+
+  return (
+    <>
+      <TopNavigator page={PAGE.PastOrders} />
+      <div className="page-container past-orders-page-container">
+        <div className="past-orders-page-title">
+          <span>Past Orders</span>
+        </div>
+        <div className="past-orders-page-content">
+          {props.user?.pastOrders ? (
+            props.user.pastOrders.map((order, index) => (
+              <React.Fragment key={`entry-${index}`}>
+                <div className="past-orders-page-entry d-flex align-items-top">
+                  <img src={mcdonalds} alt="store" />
+                  <div>
+                    <Link to="/store" className="invisible-link">
+                      <p className="past-orders-page-entry-store">
+                        McDonald's (Marine Drive)
+                      </p>
+                    </Link>
+                    <p className="past-orders-page-entry-time">{order.time}</p>
+                    <p className="past-orders-page-entry-summary">
+                      3 items for 100.00 USD
+                    </p>
+                  </div>
+                  <div className="past-orders-page-entry-buttons">
+                    <Button className="quick-bite-button quick-bite-bg">
+                      View Receipt
+                    </Button>
+                    <Link to="/store" className="invisible-link">
+                      <Button className="quick-bite-button quick-bite-bg">
+                        View Store
+                      </Button>
+                    </Link>
+                    <Button className="quick-bite-button quick-bite-bg">
+                      Rate Your Order
+                    </Button>
+                  </div>
+                </div>
+              </React.Fragment>
+            ))
+          ) : (
+            <span>No past orders found. Feel free to look around!</span>
+          )}
+        </div>
+      </div>
+    </>
+  );
 });
